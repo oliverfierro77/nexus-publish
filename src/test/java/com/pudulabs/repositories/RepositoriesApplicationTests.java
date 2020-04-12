66d3117074609dcfbc3c63dd685dec3a36be342d
oliverfierro <ext_ofierrov@falabella.cl>
2020-04-12 03:47:02 -0400

    initial source

---
+++ src/test/java/com/pudulabs/repositories/RepositoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pudulabs.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
